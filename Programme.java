ddfa3ef0bb5ed44c68248d19285ac5a190fb8a14
Alexandre Chevalier <dev.alexandre.chevalier@gmail.com>
2023-10-23 14:02:59 +0200

    feat: parsing numbers

--- Programme.java
+++ Programme.java
@@ -2,4 +2,8 @@ public class Programme {
   public static void main(String[] args) {
     System.out.println("Hello Git!");
   }
+
+  public String parseNumber(Integer number) {
+    return number.toString();
+  }
 }
